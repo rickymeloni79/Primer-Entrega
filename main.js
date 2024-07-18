205f5e8e07cbb4c84fea623b1d9efb544896b223
rickymeloni79 <157983128+rickymeloni79@users.noreply.github.com>
2024-07-18 13:48:18 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,192 @@
+
+
+let saludo
+    alert("Hola Bienvenido/a al Resto de Walter \nHas click en ok para ver le menu")
+
+
+function login(){
+    confirm("Ingresa tus datos para continuar")
+let nombre
+do{
+    nombre = prompt("Ingresa tu nombre")
+    if(nombre ==""){
+        alert("Debes introducir un nombre")
+        console.log(nombre)
+    }
+}
+while (nombre =="")
+
+let apellido
+do{
+    apellido = prompt("Ingresa tu apellido")
+    if(apellido ==""){
+        alert("Debes introducir un apellido")
+        console.log(apellido)
+    }
+ }
+while (apellido =="")
+
+let direccion
+do{
+    direccion = prompt("Nombre de la calle")
+    if(direccion ==""){
+        alert("Debes introducir una calle")
+        console.log(direccion)
+    }
+}   
+while (direccion =="")
+
+let altura
+do{
+    altura = parseInt(prompt("Indica la altura"))
+    if (isNaN(altura)){
+        alert("Debes introducir una altura")
+        console.log (altura)
+    }
+}   
+while (isNaN(altura))
+}   
+
+const cartaPlatos = ["bife con pure", "spaguetti con salsa", "ensalda cesar", "pizza", "empandas"]
+alert("MENU \nBife con pure - $1500 \nSpaguetti con salsa - $1000 \nEnsalda Cesar - $1200 \nPizza - $800 \nEmpanadas - $100 c/u")
+
+
+//let cartaPlatos
+    alert("MENU \nBife con pure - $1500 \nSpaguetti con salsa - $1000 \nEnsalda Cesar - $1200 \nPizza - $800 \nEmpanadas - $100 c/u")
+
+//let cartaBebidas
+
+//alert("MENU \nAgua con gas $150 \nGaseosa $200 \nJugo $250 \nVino $700 \nCerveza $550")
+
+
+
+let bifeConPure = 1500
+    console.log(1500)
+
+let spaguettiConSalsa = 1000
+    console.log(1000)
+
+let ensaldaCesar = 1200
+    console.log(1200)
+
+let pizza = 800
+    console.log(800)
+    
+let empanadas = 100
+    console.log(100)  
+
+
+
+
+
+
+// let continuar = true
+
+//     while(continuar){
+
+//     let platos = parseInt(prompt("Elije una option: \n 1-Bife con pure  \n 2-Spaguetti con salsa \n 3-Ensalada Cesar \n 4-Pizza \n 5-Empanadas \n 6-salir"))
+
+
+//     switch(platos){
+//         case 1:
+//             alert("Elegiste Bife con pure")
+//             break
+        
+//         case 2:
+//             alert("Elegiste Sapaguetti con salsa")
+//             break
+
+//         case 3:
+//             alert("Elegiste Ensalda Cesar")
+//             break
+
+//         case 4:
+//             alert("Elegiste Pizza")
+//             break
+
+//         case 5:
+//             alert("Elegiste Emapanadas")
+//             break
+
+//         default:
+//             alert("Debes elegir una opcion")
+        
+        
+//     }
+
+//     let confirmacion = prompt("Deseas agregar algo mas? (si/no)")
+//     if( confirmacion == "no")
+//         continuar = false
+   
+//     }
+
+   
+  
+
+
+
+//DE ACA PARA ABAJO NO VA!!!
+
+
+
+//alert ("Ahora elige una bebida \nHas click para continuar")
+
+
+
+// let continuar2 = true
+
+//     while(continuar2){
+
+// let bebidas = parseInt(prompt("Elije una option: \n 1-Agua con gas  \n 2-Gaseosa \n 3-Jugo \n 4-Vino \n 5-Cerveza \n 6-Salir"))
+
+
+//     switch(bebidas){
+//         case 1:
+//             alert("Agua con gas")
+//             break
+        
+//         case 2:
+//             alert("Gaseosa")
+//             break
+
+//         case 3:
+//             alert("Jugo")
+//             break
+
+//         case 4:
+//             alert("Vino")
+//             break
+
+//         case 5:
+//             alert("Cerveza")
+//             break
+
+//         default:
+//             alert("Debes elegir una opcion")
+        
+
+//     }
+   
+//     let confirmacion = prompt("Deseas agregar algo mas? (si/no)")
+//     if( confirmacion == "no")
+//         continuar2 = false
+   
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+    
+
