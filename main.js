a5d92ffa25184d0397430380dc7f0ea67234335b
rickymeloni79 <157983128+rickymeloni79@users.noreply.github.com>
2024-07-19 18:29:21 -0300

    quinto commit

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 
-let saludo
-    alert("Hola Bienvenido/a al Resto de Walter \nHas click en ok para ver le menu")
-
+//FUNCIONES
 
 function login(){
     confirm("Ingresa tus datos para continuar")
@@ -46,28 +44,56 @@ while (isNaN(altura))
 
 
 
+function aceptaPedido(){   
+
+    let confirmar = true // Mostrar el total al usuario
+
+    confirm("El total de tu pedido es: $" + total +" "+ "\nConfirma su pedido?")
+    if(confirmar == true){
+        alert("Tu pedido llegara en 1 hs aprox" + "\nGracias")}
+    else
+        alert("No importa sera la proxima, gracias")
+    }
+
+
+
+
+    
+
+
+
+
+//Saludo INICIAL
+
+let saludo
+    alert("Hola Bienvenido/a al Resto de Walter \nHas click en ok para ver le menu")    
+
+
+
+//MENU
+
 const cartaPlatos = ["bife con pure", "spaguetti con salsa", "ensalda cesar", "pizza", "empandas"]
 alert("MENU \nBife con pure - $1500 \nSpaguetti con salsa - $1000 \nEnsalda Cesar - $1200 \nPizza - $800 \nEmpanadas - $100 c/u")
 
 
 
-
-
+//LLAMADO FUNCION login()
 
 login()
 
 
 
-let continuar = true;
-let total = 0;
+
+let continuar = true
+let total = 0
     
 // Asignar precios a cada plato
 const precios = {
     1: 1500, // Precio del Bife con puré
     2: 1000,  // Precio del Spaguetti con salsa
-    3: 1200,  // Precio de la Ensalada César
-    4: 800,  // Precio de la Pizza
-    5: 100  // Precio de las Empanadas
+    3: 1200, // Precio de la Ensalada César
+    4: 800, // Precio de la Pizza
+    5: 100,  // Precio de las Empanadas
 }
 
 
@@ -77,96 +103,54 @@ const precios = {
     
         switch(platos){
             case 1: 
-                alert("Elegiste Bife con puré");
-                total += precios[1]; // Sumar el precio al total
-                break;
+                alert("Elegiste Bife con puré")
+                total += precios[1] // Sumar el precio al total
+                break
             
             case 2:
-                alert("Elegiste Spaguetti con salsa");
-                total += precios[2]; // Sumar el precio al total
-                break;
+                alert("Elegiste Spaguetti con salsa")
+                total += precios[2] // Sumar el precio al total
+                break
     
             case 3:
-                alert("Elegiste Ensalada César");
-                total += precios[3]; // Sumar el precio al total
-                break;
+                alert("Elegiste Ensalada César")
+                total += precios[3] // Sumar el precio al total
+                break
     
             case 4:
-                alert("Elegiste Pizza");
-                total += precios[4]; // Sumar el precio al total
-                break;
+                alert("Elegiste Pizza")
+                total += precios[4] // Sumar el precio al total
+                break
     
             case 5:
-                alert("Elegiste Empanadas");
-                total += precios[5]; // Sumar el precio al total
-                break;
+                alert("Elegiste Empanadas")
+                total += precios[5] // Sumar el precio al total
+                break
     
     
             default:
-                alert("Debes elegir una opción válida");
-                break;
+                alert("Debes elegir una opción válida")
+                break
         }
     
         let confirmacion = prompt("Deseas agregar algo mas? (si/no)")
-        if( confirmacion == "no")
+        if( confirmacion == "no"){
             continuar = false
             }
 
+        }
+
+
+//LLAMDO FUNCION aceptaPedido()  
+
+  aceptaPedido()
     
-    // Mostrar el total al usuario
-    alert("El total de tu pedido es: $" + total + "\nLlegara en 1 hs aprox" + "\nGracias");
 
 
 
 
 
-//DE ACA PARA ABAJO NO VA!!!
 
-
-
-//alert ("Ahora elige una bebida \nHas click para continuar")
-
-
-
-// let continuar2 = true
-
-//     while(continuar2){
-
-// let bebidas = parseInt(prompt("Elije una option: \n 1-Agua con gas  \n 2-Gaseosa \n 3-Jugo \n 4-Vino \n 5-Cerveza \n 6-Salir"))
-
-
-//     switch(bebidas){
-//         case 1:
-//             alert("Agua con gas")
-//             break
-        
-//         case 2:
-//             alert("Gaseosa")
-//             break
-
-//         case 3:
-//             alert("Jugo")
-//             break
-
-//         case 4:
-//             alert("Vino")
-//             break
-
-//         case 5:
-//             alert("Cerveza")
-//             break
-
-//         default:
-//             alert("Debes elegir una opcion")
-        
-
-//     }
-   
-//     let confirmacion = prompt("Deseas agregar algo mas? (si/no)")
-//     if( confirmacion == "no")
-//         continuar2 = false
-   
-//     }
 
 
 
