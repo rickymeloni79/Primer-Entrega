d1bbea14f063b4204382633a62c022640bcafc11
rickymeloni79 <157983128+rickymeloni79@users.noreply.github.com>
2024-07-28 10:32:20 -0300

    decimoprimer commit

--- main.js
+++ main.js
@@ -1,163 +1,114 @@
+//Variables
+
+let series = 4
+let reps = 12
+let descanso = 2
 
 
-//FUNCIONES
+//Array
 
-//Funcion Log in
+const ejercicio =["Press bco plano", "Dominadas", "Press militar", "Curl biceps", "Ext triceps", "Setadillas"]
 
-function login(){
-    confirm("Ingresa tus datos para continuar")
-let nombre
-do{
-    nombre = prompt("Ingresa tu nombre")
-    if(nombre ==""){
-        alert("Debes introducir un nombre")
-        
+
+//Functions
+
+
+//Funcion datos usuario
+function datosUser(nombre, email) {
+    while (nombre === "") {
+        alert("Debes ingresar tu nombre")
+        nombre = prompt("Ingresa tu nombre")
     }
-}
-while (nombre =="")
-
-let apellido
-do{
-    apellido = prompt("Ingresa tu apellido")
-    if(apellido ==""){
-        alert("Debes introducir un apellido")        
-    }
- }
-while (apellido =="")
-
-let direccion
-do{
-    direccion = prompt("Nombre de la calle")
-    if(direccion ==""){
-        alert("Debes introducir una calle")
-    }
-}   
-while (direccion =="")
-
-let altura
-do{
-    altura = parseInt(prompt("Indica la altura"))
-    if (isNaN(altura)){
-        alert("Debes introducir una altura")
-    }
-}   
-while (isNaN(altura))
-}   
-
-
-
-
-
-//Saludo INICIAL
-
-let saludo
-    alert("Hola Bienvenido/a al Resto de Walter \nHas click en ok para ver le menu")    
-
-
-
-//MENU
-
-const cartaPlatos = ["bife con pure", "spaguetti con salsa", "ensalda cesar", "pizza", "empandas"]
-alert("MENU \nBife con pure - $1500 \nSpaguetti con salsa - $1000 \nEnsalda Cesar - $1200 \nPizza - $800 \nEmpanadas - $100 c/u")
-
-
-
-//LLAMADO FUNCION login()
-
-login()
-
-
-
-
-let continuar = true
-let total = 0
     
-// Asignar precios a cada plato
-const precios = {
-    1: 1500, // Precio del Bife con puré
-    2: 1000,  // Precio del Spaguetti con salsa
-    3: 1200, // Precio de la Ensalada César
-    4: 800, // Precio de la Pizza
-    5: 100,  // Precio de las Empanadas
+    while (email === "") {
+        alert("Debes ingresar tu email")
+        email = prompt("Ingresa tu email")    } 
 }
 
 
-    while(continuar){
-    
-        let platos = parseInt(prompt("Elije una opción: \n 1-Bife con puré  \n 2-Spaguetti con salsa \n 3-Ensalada César \n 4-Pizza \n 5-Empanadas"));
-    
-        switch(platos){
-            case 1: 
-                alert("Elegiste Bife con puré")
-                total += precios[1] // Sumar el precio al total
-                break
-            
-            case 2:
-                alert("Elegiste Spaguetti con salsa")
-                total += precios[2] // Sumar el precio al total
-                break
-    
-            case 3:
-                alert("Elegiste Ensalada César")
-                total += precios[3] // Sumar el precio al total
-                break
-    
-            case 4:
-                alert("Elegiste Pizza")
-                total += precios[4] // Sumar el precio al total
-                break
-    
-            case 5:
-                alert("Elegiste Empanadas")
-                total += precios[5] // Sumar el precio al total
-                break
-    
-    
-            default:
-                alert("Debes elegir una opción válida")
-                break
-        }
-    
-        let confirmacion = prompt("Deseas agregar algo mas? (si/no)")
-        if( confirmacion == "no"){
-            continuar = false
-            }
+//Funcion Rutina
+function rutina(grupoMuscular, series, reps, descanso) {
+    alert("La rutina de hoy es: " +"\nEjercicio: "+ grupoMuscular + "\nSeries: " + series + "x" + reps + " repeticiones" + "\nDescanso: " + descanso + " min entre series");
+}
 
-        }
-        alert("El total de tu pedido es: $" + total) // Mostrar el total al usuario
+
+//Funcion enviar noticias
+function informarNoticias(email) {
+    let deseaInformacion = prompt("Deseas recibir las últimas noticias? (si/no)")
+    if (deseaInformacion === "si") {
+        alert("Gracias. Te enviaremos las últimas noticias a: " + email)
+    } else {
+        alert("No te preocupes, no recibirás noticias.")
+    }
+}
+
+
+//Saludo Inicial
+alert("Hola bienvenido a entrenamiento en linea" + "\nPreciona OK para continuar")
+
+
+
+
+//Solicitud de datos al usuario
+let nombre = prompt("Ingresa tu nombre")
+let email = prompt("Ingresa tu email")
+datosUser(nombre, email)
 
 
 
 
 
-let confirmar = prompt("Confirma su pedido? (si/no)")
-    if(confirmar ==="si"){
-        alert("Tu pedido llegara en 1 hs aprox" + "\nGracias")}
-    else {
-        alert("No importa sera la proxima. Gracias")}
-   
+let grupoMuscular = parseInt(prompt("Que grupo muscular trabajaremos hoy:" + "\n1- Pecho" + "\n2- Espalda" + "\n3- Hombros" + "\n4- Biceps" + "\n5- triceps" + "\n6- Piernas")) 
+
+while(grupoMuscular <1 || grupoMuscular >6 || isNaN(grupoMuscular)){
+    alert("introduce una opcion valida")
+    grupoMuscular = parseInt(prompt("Que grupo muscular trabajaremos hoy:" + "\n1- Pecho" + "\n2- Espalda" + "\n3- Hombros" + "\n4- Biceps" + "\n5- triceps" + "\n6- Piernas")) 
+}
+    switch (grupoMuscular){
+    case 1:
+        alert("Elegiste: Pecho" + " " +"\nHaremos: "+ " " + ejercicio[0])
+        grupoMuscular = ejercicio[0]
+        break
+
+    case 2:
+        alert("Elegiste: Espalda" + " "+ "\nHaremos: "+ " " + ejercicio[1])
+        grupoMuscular = ejercicio[1]
+        break
+
+    case 3:
+        alert("Elegiste: Hombros" + " "+ "\nHaremos: "+ " " + ejercicio[2])
+        grupoMuscular = ejercicio[2]
+        break
+
+    case 4:
+        alert("Elegiste: Biceps" + " "+ "\nHaremos: "+ " " + ejercicio[3])
+        grupoMuscular = ejercicio[3]
+        break
+
+    case 5:
+        alert("Elegiste: Triceps" + " "+ "\nHaremos: "+ " " + ejercicio[4])
+        grupoMuscular = ejercicio[4]
+        break
+
+    case 6:
+        alert("Elegiste: Piernas" + " "+ "\nHaremos: "+ " " + ejercicio[5])
+        grupoMuscular = ejercicio[5]
+        break
+
+    default:
+        break 
+
+}
 
 
+//LLamar a la funcion rutina
+rutina(grupoMuscular, series, reps, descanso)
 
 
+//Llamar a la funcion datosUser
+datosUser(nombre, email)
+alert(nombre + " Enviaremos tu rutina a: " + email + "\n Entrena duro!!!")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-    
-
+//Llamar a la funcion enviar noticias
+informarNoticias(email)
